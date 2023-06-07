7bc00f87700460179914ff788729192fc45ed0ef
IlyaHomack16 <135148682+IlyaHomack16@users.noreply.github.com>
2023-06-07 10:06:33 +0300

    Oh my god v1.1

--- qq_all_ezz.py
+++ qq_all_ezz.py
@@ -1,2 +1,13 @@
-print("Тян гей")
-print("АААААААААААААААААААААААААААААААААААААААААААААААААААААААААААААААААААААААААААААААААААААААААААА")
+import tkinter
+
+windows = tkinter.Tk()
+windows.title("Samsung Galaxy S23 Ultra")
+windows.geometry("310x490")
+windows.resizable(False, False)
+
+c = tkinter.Canvas(windows, width=310, height=490, bg="black")
+c.place(x=0, y=0)
+c1 = tkinter.Canvas(windows, width=288, height=468, bg="#70ff57")
+c1.place(x=10, y=10)
+
+windows.mainloop()
