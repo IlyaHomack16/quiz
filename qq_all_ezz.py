960ca7a14a2dcf7d86ff33b59f77331d5114049b
IlyaHomack16 <135148682+IlyaHomack16@users.noreply.github.com>
2023-06-07 12:48:14 +0300

    Oh my god v2.0

--- qq_all_ezz.py
+++ qq_all_ezz.py
@@ -1,1 +1,18 @@
-print("Я не буду делать викторину")
+import random, math
+
+quest_list = {"Сколько лет Илье?": "12", "Сколько лет Илья занимается питоном?": "2", "Кто преподаватель Ильи по питону?": "Алина", "Как зовут маму Ильи?": "Иванова Вероника Александровна", "Тян гей?": "да"}
+s = 4
+res = 0
+
+for i in range(3):
+    m = list(quest_list.keys())[random.randint(0, s)]
+    print(m)
+    j = input()
+    if j == quest_list[m]:
+        print("Ответ верный!")
+        res += 1
+    else:
+        print(f"Правильный ответ: {quest_list[m]}.")
+    quest_list.pop(m)
+    s -= 1
+print(f"Вы справились на {math.floor((res / 3) * 100)}%!")
